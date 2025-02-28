21624501abc5fdb63906f27414ab61d35880f09b
celestinryf <celestinryf@gmail.com>
2025-02-28 10:41:30 -0800

    feat: nav bar active styling, project page images

--- src/routes/Projects/index.jsx
+++ src/routes/Projects/index.jsx
@@ -27,10 +27,10 @@ import { Database } from 'lucide-react'
 
 import { Link } from 'react-router-dom';
 
-import ClubWebsiteDemo from '../../assets/images/ClubWebsite/videos/ClubWebsite demo.mov';
-import UMarketDemo from '../../assets/images/UMarket/videos/UMarket demo.mov';
-import UWealthDemo from '../../assets/images/UWealth/Videos/UWealth video.mp4';
-import Notesapp from '../../assets/images/projects/Notesapp/Screenshot 2025-02-13 211351.png';
+import ClubWebsiteDemo from '../../assets/images/ClubWebsite/images/Screenshot2.png';
+import UMarketDemo from '../../assets/images/UMarket/images/UMarket.PNG';
+import UWealthDemo from '../../assets/images/UWealth/images/Stock-Summary.png';
+import Notesapp from '../../assets/images/projects/Notesapp/Screenshot.png';
 import Urecruiter from '../../assets/images/URecruiter/Screenshot3.png';
 
 const techStackIcons = {
@@ -57,7 +57,7 @@ const techStackIcons = {
 const projects = [
     // {
     //     name: 'UFacilitator 2025',
-    //     video: UMarketDemo,
+    //     image: UMarketDemo,
     //     description: 'Led full-stack development of a worksheet creation app for UWT SET Factilitators. Led a team for migration from Google Drive documents to PostgreSQL server, training a model on this database to auto-generate worksheets given users keywords.',
     //     github: 'https://github.com/TechStartupClub/UMarket',
     //     route: '/projects/UAssignment'
@@ -71,42 +71,42 @@ const projects = [
     },
     {
         name: 'UMarket 2025',
-        video: UMarketDemo,
+        image: UMarketDemo,
         description: 'Led full-stack development of a Social media and marketplace for Univeristy of Washington students. Focused on the API connections and UI/UX.',
         github: 'https://github.com/TechStartupClub/UMarket',
         route: '/projects/UMarket'
     },
     {
         name: 'UWealth 2024',
-        video: UWealthDemo,
+        image: UWealthDemo,
         description: 'Led full-stack development of stock tracking website, focusing on development of internal API and API connections between 3rd party, internal, and client.',
         github: 'https://github.com/TechStartupClub/UWealth',
         route: '/projects/UWealth'
     },    
     {
         name: 'Club Website 2024',
-        video: ClubWebsiteDemo,
+        image: ClubWebsiteDemo,
         description: 'Developed the website for Tech Startup Club, leading a team of 3 to design and create the website.',
         github: 'https://github.com/TechStartupClub/club-website',
         route: 'https://techstartupclub.netlify.app/'
     },
     {
         name: 'Manchester United Statistical Analysis 2024',
-        // video: UWealthDemo,
+        // image: UWealthDemo,
         description: 'Built an app to analyze Manchester Uniteds ttransfer spending in the last 10 years. Used R to create graphs, to display data, and to analyze 213,000 players statistics.',
         // github: 'https://github.com/TechStartupClub/UWealth',
         route: '/projects/ManUtd'
     },
     {
         name: 'Python Calculus Calculator 2023',
-        // video: UWealthDemo,
+        // image: UWealthDemo,
         description: 'Developed a Calculus Calculator app in Pyhton with GUI built with TKinter. Able to complete operations including volume, density, matrix algebra, derivatives, integrals, and more.',
         // github: 'https://github.com/TechStartupClub/UWealth',
         route: '/projects/Calculator'
     },
     {
         name: 'Python Notes App 2023',
-        video: Notesapp,
+        image: Notesapp,
         description: 'Developed a Calculus Calculator app in Pyhton with GUI built with TKinter. Able to complete operations including volume, density, matrix algebra, derivatives, integrals, and more.',
         // github: 'https://github.com/TechStartupClub/UWealth',
         route: '/projects/NotesApp'
@@ -128,19 +128,13 @@ const ProjectCard = ({ project }) => {
     return (
         <div className={style.projectCard}>
             <Link to={project.route} className={style.cardContent}>
-                {project.video && (
+                {project.image && (
                     <div className={style.videoContainer}>
-                        <video 
-                            src={project.video} 
-                            muted 
-                            loop 
-                            playsInline
-                            autoPlay
+                        <img 
+                            src={project.image} 
+                            alt={project.name}
                             className={style.projectVideo}
-                        >
-                            Your browser does not support the video tag.
-                        </video>
-                        
+                        />
                     </div>
                 )}
                 <div className={style.projectInfo}>
